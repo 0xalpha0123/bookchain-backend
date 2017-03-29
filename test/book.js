bf6dba8a4c1ee22226a10e61ae3507dc9f59f527
wayne-th <wayned.eth@gmail.com>
2017-03-29 16:05:14 -0600

    'Adds first test. This is not a passing test.'

--- test/book.js
+++ test/book.js
@@ -1,7 +1,7 @@
-var Book = artifacts.require("./Book.sol");
+var Book = artifacts.require("../contracts/Book.sol");
 
 contract('Book', function(accounts) {
-  var varName = "name"
+  var varName = "name";
   it('should be created with a name', function() {
     return Book.deployed().then(function(bookInstance) {
       return bookInstance.create().call(varName);
